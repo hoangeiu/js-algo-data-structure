9d41f86a4ff986e07fd1abdec27324c0b73000f3
Minh Hoàng Lê <leminhhoangk3@gmail.com>
2023-03-20 15:24:16 +0700

    implement BFS and DFS

--- Data Structures/BinarySearchTree.js
+++ Data Structures/BinarySearchTree.js
@@ -53,9 +53,68 @@ class BinarySearchTree {
     if (!found) return undefined;
     return current;
   }
+
+  BFS() {
+    var data = [],
+      queue = [],
+      node = this.root;
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+
+  DFS_PreOrder() {
+    var data = [],
+      current = this.root;
+
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(current);
+
+    return data;
+  }
+
+  DFS_PostOrder() {
+    var data = [],
+      current = this.root;
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+
+    traverse(current);
+
+    return data;
+  }
+
+  DFS_InOrder() {
+    var data = [],
+      current = this.root;
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(current);
+
+    return data;
+  }
 }
 
-//     10
+//      10
 //   6       15
 // 3   8  13    20
 
@@ -68,7 +127,13 @@ tree.insert(8);
 tree.insert(3);
 tree.insert(20);
 
-console.log(tree);
+// console.log(tree.find(20));
+// console.log(tree.find(12));
 
-console.log(tree.find(20));
-console.log(tree.find(12));
+console.log(tree.BFS());
+
+console.log(tree.DFS_PreOrder());
+console.log(tree.DFS_PostOrder());
+console.log(tree.DFS_InOrder());
+
+console.log(tree);
